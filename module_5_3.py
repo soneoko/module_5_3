468704a1d6d14912fbb2bf67bf6a5ef8df8608e5
soneoko <Sofya.kayumova01@gmail.com>
2024-08-22 05:43:13 +0500

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,68 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        new_floor = int(new_floor)
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            for el in [i for i in range(1, new_floor + 1)]:
+                print(el)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return int(self.number_of_floors) == int(other.number_of_floors)
+
+    def __lt__(self, other):
+        return int(self.number_of_floors) < int(other.number_of_floors)
+
+    def __gt__(self, other):
+        return int(self.number_of_floors) > int(other.number_of_floors)
+
+    def __le__(self, other):
+        return int(self.number_of_floors) <= int(other.number_of_floors)
+
+    def __ge__(self, other):
+        return int(self.number_of_floors) >= int(other.number_of_floors)
+
+    def __ne__(self, other):
+        return int(self.number_of_floors) != int(other.number_of_floors)
+
+    def __add__(self, other):
+        return House(self.name, self.number_of_floors + other)
+
+    def __iadd__(self, other):
+        return self.__add__(other)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
